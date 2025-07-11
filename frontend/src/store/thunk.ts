5ab2d3a952bd280097e5c7ec0970c5b1b7eae4b3
Ade <adesolasolarin@gmail.com>
2025-07-11 20:18:31 +0100

    hd

--- frontend/src/store/thunk.ts
+++ frontend/src/store/thunk.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const URL_SERV = "http://localhost:3001";
+const URL_SERV = "https://ecommerce-6pbo.onrender.com";
 
 interface getEmailUserData {
   email: string;
